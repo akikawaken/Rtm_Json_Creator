f83ea841087833ba5a5b6b3c9e5ad9a41ae2e6f7
akikawa <yusei.t110@outlook.jp>
2022-12-26 19:10:52 +0900

    first commit

---
+++ sign_json_createor.bat
@@ -0,0 +1,187 @@
+@echo off
+set user=
+goto :3
+:2
+ echo -------------------
+ echo 続行すると内容が消えます。
+ echo 続行してもよろしいですか?
+ set user=
+ set /p user=(y / n)
+ if %user% == y goto 21
+ if %user% == n goto %back%
+ :21
+  exit
+:3
+ cls
+ echo 看板のjsonを作成します。
+ echo もし途中でミスをした場合は、最後に編集できるのでそこで変更してください。
+ echo -------------
+ echo textureを決めてください。(先に保存しておいた看板のpngファイル名を拡張子なしで入力してください。)
+ echo ノート:競合を避けるために、[作者名]_1のような形にするのがおすすめです。(もちろん[]は外してください)
+ set /p texture=
+ echo textureは %texture% に設定されました。
+ echo -------------
+ echo backTextureを決めてください。 使用可能:0,1,2 
+ echo  0=裏表が同じテクスチャになります。
+ echo  1=画像の右半分が裏のテクスチャになります。
+ echo  2=裏が側面の色と同じ色になります。
+ set /p backTexture=
+ echo backTextureは %backTexture% に設定されました。
+ echo -------------
+ echo heightを決めてください。 使用可能:整数と小数第二位まで(例:0.75) 
+ echo 単位は"メートル"です。
+ echo これは看板の高さになります。
+ set /p height=
+ echo heightは %height% に設定されました。
+ echo -------------
+ echo widthを決めてください。  使用可能:整数と小数第二位まで(例:2.25)
+ echo 単位は"メートル"です。
+ echo これは看板の横の長さになります。
+ set /p width=
+ echo widthは %width% に設定されました。
+ echo -------------
+ echo depthを決めてください。 使用可能:整数と小数第二位まで(例:0.375)
+ echo 単位は"メートル"です。
+ echo これは看板の奥行になります。
+ set /p depth=
+ echo depthは %depth% に設定されました。
+ echo -------------
+ echo frameを決めてください。 使用可能:整数
+ echo アニメーションしない場合は1に設定してください。
+ set /p frame=
+ echo frameは %frame% に設定されました。
+ echo -------------
+ echo animationCycleを決めてください。 使用可能:整数
+ echo アニメーションしない場合は1に設定してください。
+ set /p animationCycle=
+ echo animationCycleは %animationCycle% に設定されました。
+ echo -------------
+ echo colorを決めてください。 使用可能:6桁の整数
+ echo  -- 16 進数カラーコードを 10 進数に変換した値
+ echo わからないならば0にしましょう。
+ set /p color=
+ echo colorは %color% に設定されました。
+ echo -------------
+ echo lightValueを決めてください。 使用可能:-16~15
+ echo これは看板の発光の度合いを設定します。
+ echo -16 ランダムに点滅する
+ echo -15~-1 RS信号が入っている時、発光する(数字は発光の強さ)
+ echo 0 発行しない
+ echo 1~15 常に発光する(数字は発光の強さ)
+ set /p lightValue=
+ echo lightValueは %lightValue% に設定されました。
+ echo -------------
+ echo jsonが完成しました!
+ goto signjson
+:signjson
+ set back=signjson
+ echo ------ファイル名:signboard_%texture%.json-------
+ echo {
+ echo   "texture": "%texture%",
+ echo   "backTexture": %backTexture%,
+ echo   "height": %height%,
+ echo   "width": %width%,
+ echo   "depth": %depth%,
+ echo   "frame": %frame%,
+ echo   "animationCycle": %animationCycle%,
+ echo   "color" : %color%,
+ echo   "lightValue": %lightValue%
+ echo }
+ echo ------------------------------------------------
+ echo このjsonで満足していますか?
+ echo 編集をする場合は1,終了する場合は2を押してください。
+ set user=
+ set /p user=
+ if %user% == 1 goto signedit
+ if %user% == 2 goto 2
+ echo エラー:不明な番号
+ goto signjson
+:signedit
+ echo どの部分を編集しますか?
+ echo jsonのデータ値の名前を入力してください。(上のjsonからデータ値をコピペしてください。小文字大文字の違いでエラーになってしまいます。)
+ set edit=1
+ set user=
+ set /p user=
+ if %user% == texture goto signedit_t
+ if %user% == backTexture goto signedit_b
+ if %user% == height goto signedit_h
+ if %user% == width goto signedit_w
+ if %user% == depth goto signedit_d
+ if %user% == frame goto signedit_f
+ if %user% == animationCycle goto signedit_a
+ if %user% == color goto signedit_c
+ if %user% == lightValue goto signedit_l
+ echo エラー:不明な名前
+ goto signedit
+ :signedit_t
+ echo -------------
+ echo textureを決めてください。(先に保存しておいた看板のpngファイル名を拡張子なしで入力してください。)
+ set /p texture=
+ echo textureは %texture% に設定されました。
+ goto signjson
+ :signedit_b
+ echo -------------
+ echo backTextureを決めてください。 使用可能:0,1,2 
+ echo  0=裏表が同じテクスチャになります。
+ echo  1=画像の右半分が裏のテクスチャになります。
+ echo  2=裏が側面の色と同じ色になります。
+ set /p backTexture=
+ echo backTextureは %backTexture% に設定されました。
+ goto signjson
+ :signedit_h
+ echo -------------
+ echo heightを決めてください。 使用可能:整数と小数第二位まで(例:0.75) 
+ echo 単位は"メートル"です。
+ echo これは看板の高さになります。
+ set /p height=
+ echo heightは %height% に設定されました。
+ goto signjson
+ :signedit_w
+ echo -------------
+ echo widthを決めてください。  使用可能:整数と小数第二位まで(例:2.25)
+ echo 単位は"メートル"です。
+ echo これは看板の横の長さになります。
+ set /p width=
+ echo widthは %width% に設定されました。
+ goto signjson
+ :signedit_d
+ echo -------------
+ echo depthを決めてください。 使用可能:整数と小数第二位まで(例:0.375)
+ echo 単位は"メートル"です。
+ echo これは看板の奥行になります。
+ set /p depth=
+ echo depthは %depth% に設定されました。
+ goto signjson
+ :signedit_f
+ echo -------------
+ echo frameを決めてください。 使用可能:整数
+ echo アニメーションしない場合は1に設定してください。
+ set /p frame=
+ echo frameは %frame% に設定されました。
+ goto signjson
+ :signedit_a
+ echo -------------
+ echo animationCycleを決めてください。 使用可能:整数
+ echo アニメーションしない場合は1に設定してください。
+ set /p animationCycle=
+ echo animationCycleは %animationCycle% に設定されました。
+ goto signjson
+ :signedit_c
+ echo -------------
+ echo colorを決めてください。 使用可能:6桁の整数
+ echo  -- 16 進数カラーコードを 10 進数に変換した値
+ echo わからないならば0にしましょう。
+ set /p color=
+ echo colorは %color% に設定されました。
+ goto signjson
+ :signedit_l
+ echo -------------
+ echo lightValueを決めてください。 使用可能:-16~15
+ echo これは看板の発光の度合いを設定します。
+ echo -16 ランダムに点滅する
+ echo -15~-1 RS信号が入っている時、発光する(数字は発光の強さ)
+ echo 0 発行しない
+ echo 1~15 常に発光する(数字は発光の強さ)
+ set /p lightValue=
+ echo lightValueは %lightValue% に設定されました。
+ goto signjson
