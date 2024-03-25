6710ad9daf2d3a912400d87e28a17f1fa5ad480e
tsubaki <akikawa9616@outlook.jp>
2024-03-25 21:10:26 +0900

    update update

--- rtm_json_creator.bat
+++ rtm_json_creator.bat
@@ -2715,24 +2715,10 @@ goto selectwelcome
  set /p confirm=Y/N:
  if not %confirm% == y goto welcome
  pushd %temp%
- echo %temp%\.RJC\配下を削除中...
- rd /S /Q %temp%\.RJC\
- echo %temp%\.RJC\配下を削除しました。
- set nextpath=%random%
- echo nextpathは%nextpath%です。
- if exist %temp%\.RJC\update\%nextpath%\RtmJsonCreator.bat set nextpath=%random%
- echo %temp%\rjcupdate.tscfを作成... [%temp%\.RJC\update\%nextpath%]
- echo %temp%\.RJC\update\%nextpath%>%temp%\rjcupdate.tscf
- echo %temp%\.RJC\update\%nextpath%\を作成...
- md %temp%\.RJC\update\%nextpath%\
- pushd %temp%\.RJC\update\%nextpath%\
- curl -sLJO https://github.com/akikawaken/Rtm_Json_Creator/releases/download/update/RtmJsonCreator.bat
- echo アップデートが完了しました。
- echo RtmJsonCreatorを再起動してください。
- pushd %userprofile%
- pause
- exit /b
- 
+ del delete.bat
+ curl -sLJO https://akikawaken.github.io/RJC/VC/delete.bat
+ start delete.bat
+ exit
 :useams
  echo これは自動材質設定機能のテストです。
  echo 材質数,材質名,材質テクスチャパスを出力します。
