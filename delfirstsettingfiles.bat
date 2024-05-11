2039e4984cd3bc044cba9a6eced1592bffbb1a0f
tsubaki <akikawa9616@outlook.jp>
2024-05-12 04:16:32 +0900

    aaaba

--- delfirstsettingfiles.bat
+++ delfirstsettingfiles.bat
@@ -1,6 +1,10 @@
-del /Q %temp%\.RJC\
+rd /S /Q %temp%\.RJC\
 del /Q %temp%\.Rtm_Json_Creator.tscf
 del /Q %temp%\.ams1.tscf
 del /Q %temp%\.ams2.tscf
 del /Q %temp%\rjcupdate.tscf
 del /Q %temp%\rjcversion.tscf
+pushd %temp%
+del /q %temp%\delete.bat
+for /f %%a in ('dir /s /b *_delete.bat') do (del /q %%a)
+popd
